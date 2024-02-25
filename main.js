356935f419e06fffe14de981bf9d67ca3496e55a
silfredo ibarra <ibarrasilfredo@gmail.com>
2024-02-25 02:12:32 -0500

    I fixed the problem with the nav selector

--- main.js
+++ main.js
@@ -2,21 +2,34 @@ document.addEventListener("DOMContentLoaded", () => {
     const sections = document.querySelectorAll("section");
     const navLinks = document.querySelectorAll("nav a");
 
-    window.onscroll = () => {
-        let current = "";
-
-        sections.forEach((section) => {
-            const sectionTop = section.offsetTop;
-            if (pageYOffset >= sectionTop - 50) {
-                current = section.getAttribute("id");
-            }
-        });
-
+    function activateLink(currentId) {
         navLinks.forEach((link) => {
             link.classList.remove("active");
-            if (link.href.includes(current)) {
+            if (link.getAttribute("href") === `#${currentId}`) {
                 link.classList.add("active");
             }
         });
+    }
+
+    window.onscroll = () => {
+        let currentSectionId = ""; // Inicialmente no hay ninguna sección activa
+        sections.forEach((section) => {
+            const sectionTop = section.offsetTop;
+            if (pageYOffset >= sectionTop - 50) {
+                currentSectionId = section.getAttribute("id");
+            }
+        });
+
+        if (currentSectionId) {
+            activateLink(currentSectionId);
+        } else {
+            // Si no hay ninguna sección que cumpla el criterio, activar por defecto el primer enlace
+            // Esto asume que el primer enlace es 'Home' o el inicio de tu sitio.
+            const firstSectionId = sections[0].getAttribute("id");
+            activateLink(firstSectionId);
+        }
     };
+
+    // Activa el primer enlace al cargar la página si está en la parte superior
+    activateLink(sections[0].getAttribute("id"));
 });
